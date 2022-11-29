c5e344bfd8100ae3b0d8cc678eec6bec2aab5b3e
imranyy <imranmatano6@gmail.com>
2022-11-29 22:08:11 +0300

    toggle home to business section done

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,10 +1,55 @@
 import React from 'react';
 
 function ContactUs(props) {
+    const sendMessage=async()=>{
+
+    }
     return (
         <div className='contact-us'>
             <div className='heading'>
-
+                <h2>Contact Us</h2>
+                <p>Interested in any of our services? Tell us!</p>
+            </div>
+            <div className='grid-contact'>
+                <div className='grid-contact-item'>
+                    <h2>Find Us on</h2>
+                    <p>Talk to us whenever, wherever, we’ll listen.</p> 
+                    <ul>
+                        <li>sales@globilinksolutions.com</li>
+                        <li>www.globilinksolutions.com/</li>
+                        <li>+254 799 507 509</li>
+                        <li>+254 793 486 784</li>
+                    </ul>
+                </div>
+                <div className='grid-contact-item'>
+                    <form onSubmit={sendMessage}>
+                        <div className='input'>
+                            <div className='opt'>
+                                <label>Name</label>
+                                <input type='text' required/>
+                            </div>
+                            <div className='opt'>
+                                <label>Location</label>
+                                <input type='text' required/>
+                            </div>
+                        </div>
+                        <div className='input'>
+                            <div className='opt'>
+                                <label>Email Address</label>
+                                <input type='email' required/>
+                            </div>
+                            <div className='opt'>
+                                <label>Phone Number</label>
+                                <input type='number' required/>
+                            </div>
+                        </div>
+                        <div className='opt'>
+                            <label>Message</label>
+                            <textarea required></textarea>
+                        </div>
+                        <button>Send Message</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,15 +1,78 @@
 import React from 'react';
 
 function Plans(props) {
+    const showBusiness=()=>{
+        document.querySelector('.plans1').style.display='block';
+        document.querySelector('.plans').style.display="none";
+    }
+    const showHome=()=>{
+        document.querySelector('.plans1').style.display='none';
+        document.querySelector('.plans').style.display="block";
+    }
     return (
         <>
+        {/* Home */}
             <div className='plans'>
                 <h1>Internet Plans</h1>
                 <div className='plans-button'>
-                    <button className='btn1'>Home</button>
-                    <button className='btn2'>Business</button>
+                    <button className='btn1' >Home</button>
+                    <button className='btn2' onClick={showBusiness}>Business</button>
                 </div>
                 <div className='grid-plans'>
+                    <div className='grid-item-plan1'>
+                        <h2>BRONZE</h2>
+                        <p>Perfect to get started</p>
+                        <h1>6mbps</h1>
+                        <div className='content'>
+                            <ul>
+                                <li>Unlimited internet</li>
+                                <li>Customer data protection</li>
+                                <li>HD video streaming and VoIP calls</li>
+                                <li>HD surveillance streaming</li>
+                            </ul>
+                        </div>
+                        <button className='btn1'>KSH 2500/mo</button>
+                    </div>
+
+                    <div className='grid-item-plan2'>
+                        <h2>BRONZE</h2>
+                        <p>Perfect to get started</p>
+                        <h1>6mbps</h1>
+                        <div className='content'>
+                            <ul>
+                                <li>Unlimited internet</li>
+                                <li>Customer data protection</li>
+                                <li>HD video streaming and VoIP calls</li>
+                                <li>HD surveillance streaming</li>
+                            </ul>
+                        </div>
+                        <button className='btn2'>KSH 3000/mo</button>
+                    </div>
+
+                    <div className='grid-item-plan3'>
+                        <h2>BRONZE</h2>
+                        <p>Perfect to get started</p>
+                        <h1>6mbps</h1>
+                        <div className='content'>
+                            <ul>
+                                <li>Unlimited internet</li>
+                                <li>Customer data protection</li>
+                                <li>HD video streaming and VoIP calls</li>
+                                <li>HD surveillance streaming</li>
+                            </ul>
+                        </div>
+                        <button className='btn1'>KSH 10999/mo</button>
+                    </div>
+                </div>
+            </div>
+            {/* Business */}
+            <div className='plans1' style={{display:'none'}}>
+                <h1>Internet Plans</h1>
+                <div className='plans1-button'>
+                    <button className='btn2' onClick={showHome}>Home</button>
+                    <button className='btn1'>Business</button>
+                </div>
+                <div className='grid-plans1'>
                     <div className='grid-item-plan1'>
                         <h2>BRONZE</h2>
                         <p>Perfect to get started</p>
