1fdc5ace792e143ad99bee4b50b6e66de40469f2
imranyy <imranmatano6@gmail.com>
2022-12-07 10:32:08 +0300

    made navbar and hero image responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,27 @@ function Navbar(props) {
         document.querySelector('.plans1').style.display='none';
         document.querySelector('.plans').style.display="block";
         document.querySelector('.plans').scrollIntoView();
+        closeMenu()
     }
     const services=()=>{
         document.querySelector('.service').scrollIntoView();
+        closeMenu()
     }
     const business=()=>{
         document.querySelector('.plans1').style.display='block';
         document.querySelector('.plans').style.display="none";
         document.querySelector('.plans1').scrollIntoView();
+        closeMenu()
     }
     const contact=()=>{
         document.querySelector('.contact-us').scrollIntoView();
+        closeMenu()
+    }
+    const showMenu=()=>{
+        document.querySelector('.nav-menu').style.display="block";
+    }
+    const closeMenu=()=>{
+        document.querySelector('.nav-menu').style.display="none";
     }
     return (
         <>
@@ -26,8 +36,18 @@ function Navbar(props) {
                     <li onClick={services}>Services</li>
                     <li onClick={business}>Business</li>
                     <li onClick={contact}>call +254793486784</li>
+                    <li className='show-menu' onClick={showMenu}>Menu</li>
                 </ul>
-            </nav>   
+            </nav>  
+            <div className='nav-menu'>
+                <div className='close-menu' onClick={closeMenu}>Close</div>
+                <ul>
+                    <li onClick={plans}>Plans</li>
+                    <li onClick={services}>Services</li>
+                    <li onClick={business}>Business</li>
+                    <li onClick={contact}>call +254793486784</li>
+                </ul>
+            </div> 
         </>
     );
 }
