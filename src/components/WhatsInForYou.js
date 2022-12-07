a4977261d30fbd9d62f10961d941cf7b0780dd62
imranyy <imranmatano6@gmail.com>
2022-12-07 20:17:57 +0300

    done with responsiveness

--- src/components/WhatsInForYou.js
+++ src/components/WhatsInForYou.js
@@ -6,46 +6,52 @@ import img4 from '../media/Rectangle 13.png';
 
 function WhatsInForYou(props) {
     const show1=()=>{
-        document.querySelector('.grid-item-whats1').style.display='block';
         document.querySelector('.img1').style.display='none';
+        dontShow2()
+        dontShow3()
+        dontShow4()
     }
     const show2=()=>{
-        document.querySelector('.grid-item-whats2').style.display='block';
         document.querySelector('.img2').style.display='none';
+        dontShow1()
+        dontShow3()
+        dontShow4()
     }
     const show3=()=>{
-        document.querySelector('.grid-item-whats3').style.display='block';
         document.querySelector('.img3').style.display='none';
+        dontShow1()
+        dontShow2()
+        dontShow4()
     }
     const show4=()=>{
-        document.querySelector('.grid-item-whats4').style.display='block';
         document.querySelector('.img4').style.display='none';
+        dontShow1()
+        dontShow2()
+        dontShow3()
     }
     const dontShow1=()=>{
-        document.querySelector('.grid-item-whats1').style.display='none';
         document.querySelector('.img1').style.display='block';
     }
     const dontShow2=()=>{
-        document.querySelector('.grid-item-whats2').style.display='none';
         document.querySelector('.img2').style.display='block';
     }
     const dontShow3=()=>{
-        document.querySelector('.grid-item-whats3').style.display='none';
         document.querySelector('.img3').style.display='block';
     }
     const dontShow4=()=>{
-        document.querySelector('.grid-item-whats4').style.display='none';
         document.querySelector('.img4').style.display='block';
     }
     return (
-       <div className='whats-in'>
+       <>
+        <div className='whats-in'>
             <div className='grid-whats'>
-                <div style={{cursor:'pointer'}}><img className='img1' onClick={show1}  onMouseOver={show1} onMouseLeave={dontShow1} src={img1} alt='..'/><div style={{display:'none'}} className='grid-item-whats1'><p>Unlimited Internet and VoIP calls</p></div></div>
-                <div style={{cursor:'pointer'}}><img className='img2' onClick={show2} onMouseOver={show2} onMouseLeave={dontShow2} src={img2} alt='..'/><div style={{display:'none'}} className='grid-item-whats2'><p>Customer Data Protection</p></div></div>
-                <div style={{cursor:'pointer'}}><img className='img3' onClick={show3} onMouseOver={show3} onMouseLeave={dontShow3} src={img3} alt=".."/><div style={{display:'none'}} className='grid-item-whats3'><p>HD Surveillance Streaming</p></div></div>
-                <div style={{cursor:'pointer'}}><img className='img4' onClick={show4} onMouseOver={show4} onMouseLeave={dontShow4} src={img4} alt=".."/><div style={{display:'none'}} className='grid-item-whats4'><p>HD Video Streaming</p></div></div>
+                <div className='grid-item-whats1' onClick={show1}><img className='img1' src={img1} alt='..'/><p>Unlimited Internet and VoIP calls</p></div>
+                <div className='grid-item-whats2' onClick={show2}><img className='img2' src={img2} alt='..'/><p>Customer Data Protection</p></div>
+                <div className='grid-item-whats3' onClick={show3}><img className='img3' src={img3} alt=".."/><p>HD Surveillance Streaming</p></div>
+                <div className='grid-item-whats4' onClick={show4}><img className='img4' src={img4} alt=".."/><p>HD Video Streaming</p></div>
             </div>
        </div>
+       </>
     );
 }
 
