1b18cba6caa837d53e886fae66420ba449909e1b
imranyy <imranmatano6@gmail.com>
2023-02-17 22:49:57 +0300

    adding modals

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import vect1 from '../media/arcticons_internet-speed-meter-lite.png';
 import vect2 from '../media/carbon_manage-protection.png';
 import vect3 from '../media/Vector0.png';
@@ -7,7 +7,29 @@ import vect5 from '../media/arcticonWhite.png';
 import vect6 from '../media/carbonWhite.png';
 import vect7 from '../media/fluentWhite.png';
 import vect8 from '../media/cctvWhite.png';
-import {motion} from 'framer-motion'
+import {motion} from 'framer-motion';
+import Modal from 'react-modal';
+import { AiOutlineClose } from 'react-icons/ai';
+
+const customStyles={
+    content:{
+      background:'#183060',
+        position:'fixed',
+        top:'20vh',
+        left:'10vw',
+        right:'10vw',
+        bottom:'15vh',
+        color:'white',
+        zIndex:'200',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifuContent:'center',
+        lineHeight:'25px',
+      transform:'translate(-50%.-50%)'
+    },
+  }
+
 function Plans(pro1s){
     const showBusiness=()=>{
         document.querySelector('.plans1').style.display='block';
@@ -19,8 +41,116 @@ function Plans(pro1s){
         document.querySelector('.plans').style.display="block";
         document.querySelector('.move-up-img').style.display='block';
     }
+    //modal1
+    const [modalIsOpen1,setIsOpen1]=useState(false);
+    const openModal1=()=>{
+        setIsOpen1(true)
+    }
+    const closeModal1=()=>{
+        setIsOpen1(false)
+    }
+    //modal2
+    const [modalIsOpen2,setIsOpen2]=useState(false);
+    const openModal2=()=>{
+        setIsOpen2(true)
+    }
+    const closeModal2=()=>{
+        setIsOpen2(false)
+    }
+    //modal3
+    const [modalIsOpen3,setIsOpen3]=useState(false);
+    const openModal3=()=>{
+        setIsOpen3(true)
+    }
+    const closeModal3=()=>{
+        setIsOpen3(false)
+    }
+    //modal4
+    const [modalIsOpen4,setIsOpen4]=useState(false);
+    const openModal4=()=>{
+        setIsOpen4(true)
+    }
+    const closeModal4=()=>{
+        setIsOpen4(false)
+    }
+    //modal5
+    const [modalIsOpen5,setIsOpen5]=useState(false);
+    const openModal5=()=>{
+        setIsOpen5(true)
+    }
+    const closeModal5=()=>{
+        setIsOpen5(false)
+    }
+    //modal6
+    const [modalIsOpen6,setIsOpen6]=useState(false);
+    const openModal6=()=>{
+        setIsOpen6(true)
+    }
+    const closeModal6=()=>{
+        setIsOpen6(false)
+    }
     return (
         <>
+        {/*plans modals*/} 
+        <Modal
+             isOpen={modalIsOpen1}
+             style={customStyles}
+        >
+            <AiOutlineClose onClick={closeModal1} size={25} style={{position:'absolute',cursor:'pointer',top:'20px',right:'20px'}}/>
+            <h2>BRONZE</h2><br/>
+            <p>Perfect to get started</p>
+            <h3>6mbps</h3>
+        </Modal>
+
+        <Modal
+             isOpen={modalIsOpen2}
+             style={customStyles}
+        >
+            <AiOutlineClose onClick={closeModal2} size={25} style={{position:'absolute',cursor:'pointer',top:'20px',right:'20px'}}/>
+            <h2>SILVER</h2><br/>
+            <p>Perfect for domestic use</p>
+            <h3>12mbps</h3>
+        </Modal>
+
+        <Modal
+             isOpen={modalIsOpen3}
+             style={customStyles}
+        >
+            <AiOutlineClose onClick={closeModal3} size={25} style={{position:'absolute',cursor:'pointer',top:'20px',right:'20px'}}/>
+            <h2>GOLD</h2><br/>
+            <p>Awesome for remote work</p>
+            <h3>20mbps</h3>
+        </Modal>
+
+        {/** Business modals */}
+        <Modal
+             isOpen={modalIsOpen4}
+             style={customStyles}
+        >
+            <AiOutlineClose onClick={closeModal4} size={25} style={{position:'absolute',cursor:'pointer',top:'20px',right:'20px'}}/>
+            <h2>BRONZE</h2><br/>
+            <p>Perfect to get started</p>
+            <h3>6mbps</h3>
+        </Modal>
+        <Modal
+             isOpen={modalIsOpen5}
+             style={customStyles}
+        >
+            <AiOutlineClose onClick={closeModal5} size={25} style={{position:'absolute',cursor:'pointer',top:'20px',right:'20px'}}/>
+            <h2>SILVER</h2><br/>
+            <p>Perfect for domestic use</p>
+            <h3>15mbps</h3>
+        </Modal>
+        <Modal
+             isOpen={modalIsOpen6}
+             style={customStyles}
+        >
+            <AiOutlineClose onClick={closeModal6} size={25} style={{position:'absolute',cursor:'pointer',top:'20px',right:'20px'}}/>
+            <h2>GOLD</h2><br/>
+            <p>Awesome for remote work</p>
+            <h3>20mbps</h3>
+        </Modal>
+
         {/* Home */}
             <motion.div 
                 className='plans'
@@ -46,7 +176,7 @@ function Plans(pro1s){
                                 <li><img src={vect4} alt='...'/>  HD surveillance streaming</li>
                             </ul>
                         </div>
-                        <button className='btn1'>KSH 2500/mo</button>
+                        <button className='btn1' onClick={openModal1}>KSH 2500/mo</button>
                     </div>
 
                     <div className='grid-item-plan2'>
@@ -61,7 +191,7 @@ function Plans(pro1s){
                                 <li><img src={vect8} alt='...'/>  HD surveillance streaming</li>
                             </ul>
                         </div>
-                        <button className='btn2'>KSH 3000/mo</button>
+                        <button className='btn2' onClick={openModal2}>KSH 3000/mo</button>
                     </div>
 
                     <div className='grid-item-plan3'>
@@ -76,16 +206,17 @@ function Plans(pro1s){
                                 <li><img src={vect4} alt='...'/>  HD surveillance streaming</li>
                             </ul>
                         </div>
-                        <button className='btn1'>KSH 10999/mo</button>
+                        <button className='btn1' onClick={openModal3}>KSH 10999/mo</button>
                     </div>
                 </div>
             </motion.div>
+
             {/* Business */}
             <div className='plans1' style={{display:'none'}}>
                 <h1>Internet Plans</h1>
                 <div className='plans1-button'>
                     <button className='btn2' onClick={showHome}>Home</button>
-                    <button className='btn1'>Business</button>
+                    <button className='btn1' onClick={showBusiness}>Business</button>
                 </div>
                 <div className='grid-plans1'>
                     <div className='grid-item-plan1'>
@@ -100,7 +231,7 @@ function Plans(pro1s){
                                 <li><img src={vect4} alt='...'/>  HD surveillance streaming</li>
                             </ul>
                         </div>
-                        <button className='btn1'>KSH 3000/mo</button>
+                        <button className='btn1' onClick={openModal4}>KSH 3000/mo</button>
                     </div>
 
                     <div className='grid-item-plan2'>
@@ -115,7 +246,7 @@ function Plans(pro1s){
                                 <li><img src={vect8} alt='...'/>  HD surveillance streaming</li>
                             </ul>
                         </div>
-                        <button className='btn2'>KSH 5499/mo</button>
+                        <button className='btn2' onClick={openModal5}>KSH 5499/mo</button>
                     </div>
 
                     <div className='grid-item-plan3'>
@@ -130,7 +261,7 @@ function Plans(pro1s){
                                 <li><img src={vect4} alt='...'/>  HD surveillance streaming</li>
                             </ul>
                         </div>
-                        <button className='btn1'>KSH 15499/mo</button>
+                        <button className='btn1' onClick={openModal6}>KSH 15499/mo</button>
                     </div>
                 </div>
             </div>
