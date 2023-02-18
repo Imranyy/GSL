270defaaf497b1d7794dc7826def8a87bf9cba0f
imranyy <imranmatano6@gmail.com>
2023-02-18 12:20:32 +0300

    added the hover effect on the 'what's in for you section'

--- src/components/WhatsInForYou.js
+++ src/components/WhatsInForYou.js
@@ -27,33 +27,37 @@ function WhatsInForYou(props) {
                     <motion.p className='text' initial={{scale:0}} whileInView={{scale:1}} transition={{duration:0.5}} viewport={{once:true}}>
                     Unlimited Internet and VoIP calls
                     </motion.p>
-                    <p className='sec-text' style={{display:'none'}}>
-                        nice
-                    </p>
+                    <div className='sec-text' style={{display:'none', margin:'130px 20px',textAlign:'left',color:'white'}}>
+                        <h3>Unlimited Internet and VoIP calls</h3><br/>
+                        We have the equipment and the telecommunication line access required to have a point of presence on the internet for the geographic area served.
+                    </div>
                 </div>
                 <div className='grid-item-whats2' onClick={show2} onMouseOver={show2}>
                     <motion.p  className='text1' initial={{scale:1}} whileHover={{scale:0}} transition={{duration:0.5}} viewport={{once:true}}>
                         Customer Data Protection
                     </motion.p>
-                    <p className='sec-text1' style={{display:'none'}}>
-                       Woow
-                    </p>
+                    <div className='sec-text1' style={{display:'none', margin:'130px 20px',textAlign:'left',color:'white'}}>
+                        <h2>Customer Data Protection</h2><br/>
+                       We have the equipment and the telecommunication line access required to have a point of presence on the internet for the geographic area served.
+                    </div>
                 </div>
                 <div className='grid-item-whats3' onClick={show3} onMouseOver={show3}>
                     <motion.p  className='text2' initial={{scale:1}} whileHover={{scale:0}} transition={{duration:0.5}} viewport={{once:true}}>
                         HD Surveillance Streaming
                     </motion.p>
-                    <p className='sec-text2' style={{display:'none'}}>
-                        People
-                    </p>
+                    <div className='sec-text2' style={{display:'none', margin:'130px 20px',textAlign:'left',color:'white'}}>
+                        <h2>HD Surveillance Streaming</h2><br/>
+                        We have the equipment and the telecommunication line access required to have a point of presence on the internet for the geographic area served.
+                    </div>
                 </div>
                 <div className='grid-item-whats4' onClick={show4} onMouseOver={show4}>
                     <motion.p  className='text3' initial={{scale:1}} whileHover={{scale:0}} transition={{duration:0.5}} viewport={{once:true}}>
                         HD Video Streaming
                     </motion.p>
-                    <p className='sec-text3' style={{display:'none'}}>
-                        Hello friend
-                    </p>
+                    <div className='sec-text3' style={{display:'none', margin:'130px 20px',textAlign:'left',color:'white'}}>
+                        <h2>HD Video Streaming</h2><br/>
+                        We have the equipment and the telecommunication line access required to have a point of presence on the internet for the geographic area served.
+                    </div>
                 </div>
             </div>
        </div>
